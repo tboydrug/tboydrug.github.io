d0c55c10b80262ed7d0f67e155a98096e22ca215
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-26 11:58:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+document.getElementById("playBtn").addEventListener("click", function () {
+    // Измените URL на тот, на который вы хотите перенаправить пользователя
+    window.location.href = "file:///C:/Users/malum/Desktop/webapp/game.html";
+});
