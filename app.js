cf011fbf1a539760b2b05e3bb0adf4c5ae7e1a23
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-26 12:40:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 document.getElementById("playBtn").addEventListener("click", function () {
-    // Измените URL на тот, на который вы хотите перенаправить пользователя
-    window.location.href = "file:///C:/Users/malum/Desktop/webapp/game.html";
+    window.location.href = "https://tboydrug.github.io/game";
 });
