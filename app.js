87445b81e498e22f1234fe16da4497d2cc7c6f5b
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-27 06:25:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-document.getElementById("playBtn").addEventListener("click", function () {
+let btn = document.getElementById("playBtn");
+btn.addEventListener("click", function ()
+{
     window.location.href = "https://tboydrug.github.io/game";
+});
+
+Telegram.WebApp.onEvent("playClick", function () {
+
+    let clickEvent = new Event("click");
+
+    btn.dispatchEvent(clickEvent);
+
+    tg.sendData({ buttonClicked: "playBtn" });
 });
