c56f14e4a57e4afa3a7a63f9a2bfa8525dfb0aa6
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-28 11:36:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,10 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 let playButton = document.getElementById("playBtn");
-let backButton = document.getElementById("backBtn");
-
 playButton.addEventListener("click", function ()
 {
     window.location.href = "file:///C:/Users/malum/Desktop/webapp/game.html";
 });
-
-backButton.addEventListener("click", function () {
-    window.close();
-})
-
-Telegram.WebApp.onEvent("backClickBtn", function () {
-    let clickEvent = new Event("click");
-
-    backButton.dispatchEvent(clickEvent);
-
-    tg.sendData({ buttonClicked: "backBtn" });
-})
 
 Telegram.WebApp.onEvent("playClickBtn", function () {
 
