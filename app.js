408b5395fbc995943ed5ccd0a981d1b1232edeed
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-28 13:37:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,48 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let playButton = document.getElementById("playBtn");
-let backButton = document.getElementById("backBtn");
-playButton.addEventListener("click", function () {
+if (window.location.href.includes("index.html")) {
 
-    window.location.href = "https://tboydrug.github.io/game";
-});
+    let playButton = document.getElementById("playBtn");
 
-backButton.addEventListener("click", function () {
+    playButton.addEventListener("click", function () {
 
-    window.close();
-})
+        window.location.href = "file:///C:/Users/malum/Desktop/webapp/game.html";
+    });
+}
+else if (window.location.href.includes("game.html")) {
 
+    let backButton1 = document.getElementById("backBtn1");
+    let backButton2 = document.getElementById("backBtn2");
+
+    backButton1.addEventListener("click", function () {
+
+        history.go(-1);
+    })
+    backButton2.addEventListener("click", function () {
+
+        location.reload();
+    })
+}
 
 Telegram.WebApp.onEvent("playClickBtn", function () {
 
     let clickEvent = new Event("click");
 
-    playButton.dispatchEvent(clickEvent);
-    backButton.dispatchEvent(clickEvent);
+    if (window.location.href.includes("index.html")) {
 
-    tg.sendData({ buttonClicked: "playBtn" });
-    tg.sendData({ buttonClicked: "backBtn" });
+        playButton.dispatchEvent(clickEvent);
+
+        tg.sendData({ buttonClicked: "playBtn" });
+    }
+    else if (window.location.href.includes("game.html")) {
+
+        backButton1.dispatchEvent(clickEvent);
+
+        tg.sendData({ buttonClicked: "backBtn1" });
+
+        backButton2.dispatchEvent(clickEvent);
+
+        tg.sendData({ buttonClicked: "backBtn2" });
+    }
 });
