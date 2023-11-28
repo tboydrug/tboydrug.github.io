25f6fbd745d3e00ba2860a467cb0036101376d6c
tboydrug <79127854+tboydrug@users.noreply.github.com>
2023-11-28 14:02:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-if (window.location.href.includes("index.html")) {
+if (window.location.href.includes("https://tboydrug.github.io/index.html")) {
 
     let playButton = document.getElementById("playBtn");
 
@@ -11,7 +11,7 @@ if (window.location.href.includes("index.html")) {
         window.location.href = "https://tboydrug.github.io/game";
     });
 }
-else if (window.location.href.includes("game.html")) {
+else if (window.location.href.includes("https://tboydrug.github.io/game.html")) {
 
     let backButton1 = document.getElementById("backBtn1");
     let backButton2 = document.getElementById("backBtn2");
@@ -30,13 +30,13 @@ Telegram.WebApp.onEvent("playClickBtn", function () {
 
     let clickEvent = new Event("click");
 
-    if (window.location.href.includes("index.html")) {
+    if (window.location.href.includes("https://tboydrug.github.io/index.html")) {
 
         playButton.dispatchEvent(clickEvent);
 
         tg.sendData({ buttonClicked: "playBtn" });
     }
-    else if (window.location.href.includes("game.html")) {
+    else if (window.location.href.includes("https://tboydrug.github.io/game.html")) {
 
         backButton1.dispatchEvent(clickEvent);
 
